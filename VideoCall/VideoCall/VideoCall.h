e819b4990b3b3cfbeb5a01b99fe4c99c7a479c66
Kostiantyn Chukhas <kostiantyn.chukhas@onix-systems.com>
2022-11-28 15:50:35 +0200

    create project

---
+++ VideoCall/VideoCall/VideoCall.h
@@ -0,0 +1,18 @@
+//
+//  VideoCall.h
+//  VideoCall
+//
+//  Created by Konstantin Chukhas on 28.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VideoCall.
+FOUNDATION_EXPORT double VideoCallVersionNumber;
+
+//! Project version string for VideoCall.
+FOUNDATION_EXPORT const unsigned char VideoCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoCall/PublicHeader.h>
+
+
